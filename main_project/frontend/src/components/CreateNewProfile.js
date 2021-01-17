09ca7afb9dbd78c17200f262330aec38a7a50f37
christy lo <christylo2000@hotmail.com>
2021-01-17 03:05:04 -0800

    finished stylizing home page buttons

--- main_project/frontend/src/components/CreateNewProfile.js
+++ main_project/frontend/src/components/CreateNewProfile.js
@@ -2,10 +2,7 @@ import React from 'react'
 import "./CreateNewProfile.css"
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
 import newProfileBackground from '../images/newProfileBackground.svg';
-import { withStyles } from '@material-ui/core/styles';
 
 const left = {
     background: '#97DDAB',
@@ -31,35 +28,6 @@ const right = {
     textTransform: "capitalize"
 };
 
-const StyledButton = withStyles({
-    root: {
-        background: '#97DDAB',
-        borderRadius: 3,
-        border: 0,
-        color: 'black',
-        height: 60,
-        width: 347,
-        padding: '300 347px',
-    },
-    label: {
-        textTransform: 'capitalize',
-        size: 35
-    },
-})(Button);
-
-// const theme = createMuiTheme({
-//     palette: {
-//         primary: {
-//             // pale green
-//             main: "#97DDAB",
-//         },
-//         secondary: {
-//             // soft purple
-//             main: '#AD8DB5',
-//         },
-//     },
-// });
-
 var sectionStyle = {
     height: "850px",
     backgroundImage: "url(" + newProfileBackground + ")",
@@ -67,17 +35,6 @@ var sectionStyle = {
     // backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
 }
-
-// const styles = {
-//     root: {
-//       borderRadius: 3,
-//       border: 0,
-//       color: 'white',
-//       height: 48,
-//       padding: '0 30px',
-//       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-//     },
-// };
 
 export default function CreateNewProfile() {
 
